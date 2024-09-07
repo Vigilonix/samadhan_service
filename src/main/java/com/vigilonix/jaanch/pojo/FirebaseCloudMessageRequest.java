35511afae347b7961d19d9b104f7661a670d4760
hari_om <nischalkumarsagar@gmail.com>
2024-09-08 00:52:37 +0530

    to string for better logging

--- src/main/java/com/vigilonix/jaanch/pojo/FirebaseCloudMessageRequest.java
+++ src/main/java/com/vigilonix/jaanch/pojo/FirebaseCloudMessageRequest.java
@@ -2,11 +2,13 @@ package com.vigilonix.jaanch.pojo;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.Map;
 
 @Getter
 @Builder
+@ToString
 public class FirebaseCloudMessageRequest implements INotificationRequest{
     private final String to;
     private final String title;
