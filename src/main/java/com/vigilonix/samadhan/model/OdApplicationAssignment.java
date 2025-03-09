b42fe2ef7e31789184bb79c001ede8709c97db79
hari_om <nischalkumarsagar@gmail.com>
2025-03-10 00:04:35 +0530

    constraint

--- src/main/java/com/vigilonix/samadhan/model/OdApplicationAssignment.java
+++ src/main/java/com/vigilonix/samadhan/model/OdApplicationAssignment.java
@@ -1,42 +1,47 @@
 package com.vigilonix.samadhan.model;
 
-import com.vigilonix.samadhan.enums.ApplicationCategory;
 import com.vigilonix.samadhan.pojo.OdApplicationStatus;
-import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.Type;
 
-import java.util.List;
 import java.util.UUID;
 
 @Entity(name = "OdApplicationAssignment")
-@Table(
-        name = "od_application_assignment"
-)
 @Getter
-@AllArgsConstructor
-@Builder
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @ToString
+@Table(name = "od_application_assignment", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"application_uuid", "enquiry_officer_uuid"}, name = "UC_application_enquiry_officer")
+})
 public class OdApplicationAssignment {
     @Id
     @Column
     private UUID uuid;
+
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "application_uuid") // This should match your database column name for the application's UUID
     private OdApplication application;
+
     @Column
     private String filePath;
+
     @ManyToOne
+    @JoinColumn(name = "enquiry_officer_uuid") // This should match your database column name for the user's UUID
     private User enquiryOfficer;
+
     @Column
     @Enumerated(EnumType.STRING)
     private OdApplicationStatus status;
+
     @Column
     private Long createdAt;
+
     @Column
     private Long modifiedAt;
+
     @Column
     private String comment;
 }
